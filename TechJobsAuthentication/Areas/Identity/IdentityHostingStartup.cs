aef5de8e1ad51db268b1eb81afca0908deef29bd
las5h4 <loganasmalley@gmail.com>
2020-12-15 18:10:05 -0600

    missing data file

--- TechJobsAuthentication/Areas/Identity/IdentityHostingStartup.cs
+++ TechJobsAuthentication/Areas/Identity/IdentityHostingStartup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using TechJobsAuthentication.Areas.Identity.Data;
+using TechJobsAuthentication.Data;
 
 [assembly: HostingStartup(typeof(TechJobsAuthentication.Areas.Identity.IdentityHostingStartup))]
 namespace TechJobsAuthentication.Areas.Identity
@@ -15,12 +15,12 @@ namespace TechJobsAuthentication.Areas.Identity
         public void Configure(IWebHostBuilder builder)
         {
             builder.ConfigureServices((context, services) => {
-                services.AddDbContext<JobsDbContext>(options =>
-                    options.UseSqlServer(
-                        context.Configuration.GetConnectionString("JobsDbContextConnection")));
+                services.AddDbContext<JobDbContext>(options =>
+                    options.UseMySql(
+                        context.Configuration.GetConnectionString("DefaultConnection")));
 
                 services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                    .AddEntityFrameworkStores<JobsDbContext>();
+                    .AddEntityFrameworkStores<JobDbContext>();
             });
         }
     }
